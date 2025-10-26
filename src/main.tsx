d6a6b47b829c140719e73ed6554af43b8fe06af0
Miguel <migui.kaos14@gmail.com>
2025-10-26 11:38:18 +0100

    feat: added Sonner

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BoxUp } from './BoxUp'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster />
     <BoxUp />
   </StrictMode>,
 )
