eb8987bdd1fe45d498b47f71d1e251419f5c180b
Miguel <migui.kaos14@gmail.com>
2025-10-26 12:35:41 +0100

    feat: added Navbar component

--- src/BoxUp.tsx
+++ src/BoxUp.tsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from "react-router"
 import { appRouter } from "./router/AppRouter"
+import { Navbar } from "./Navbar"
 
 export const BoxUp = () => {
   return (
     <div className="flex flex-col">
+      <Navbar />
       <RouterProvider router={appRouter} />
     </div>
   )
