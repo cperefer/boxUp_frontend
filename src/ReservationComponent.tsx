abab1aa86d35bdedf9760d41136d07014cf06927
Miguel <migui.kaos14@gmail.com>
2025-10-27 17:35:28 +0100

    feat: added buttons and styles to ReservationComponent

--- src/ReservationComponent.tsx
+++ src/ReservationComponent.tsx
@@ -1,10 +1,34 @@
 import type { Reservation } from "./mocks/NextReservations.mock"
+import { FiEye, FiTrash } from "react-icons/fi";
+import { LuCalendarClock } from "react-icons/lu";
 
 export const ReservationComponent = ({ reservation }: { reservation: Reservation }) => {
+  const date = new Date(reservation.date);
+
+  const handleSeeReservation = () => {
+
+  }
+
+  const handleCancelReservation = () => {
+
+  }
+
   return (
-    <div key={reservation.id}>
-      <p>{reservation.date}</p>
-      <p>{reservation.type}</p>
+    <div className="w-full justify-center flex flex-row pt-2">
+      <div className="w-75 pb-1 border-b border-b-primary" key={reservation.id}>
+        <p><LuCalendarClock />{`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} - ${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`}</p>
+        <p className="ml-2">{reservation.type}</p>
+      </div>
+      <div className="flex flex-row items-center w-25 gap-2 border-b border-b-primary">
+        <button
+          onClick={handleSeeReservation}
+          className="bg-ok w-45 h-10 rounded-lg flex justify-center items-center align-middle cursor-pointer"
+        ><FiEye /></button>
+        <button
+          onClick={handleCancelReservation}
+          className="bg-error w-45 h-10 rounded-lg flex justify-center items-center align-middle cursor-pointer"
+        ><FiTrash /></button>
+      </div>
     </div>
   )
 }
--- src/mocks/NextReservations.mock.ts
+++ src/mocks/NextReservations.mock.ts
@@ -10,26 +10,26 @@ export const NextReservationsMock: Reservation[] = [
   {
     id: 1,
     type: 'CrossFit',
-    date: 1760805000
+    date: 1760805000000
   },
   {
     id: 2,
     type: 'CrossFit',
-    date: 1760977800
+    date: 1760977800000
   },
   {
     id: 3,
     type: 'CrossFit',
-    date: 1761150600
+    date: 1761150600000
   },
   {
     id: 4,
     type: 'Halterofilia',
-    date: 1761206400
+    date: 1761206400000
   },
   {
     id: 5,
     type: 'Gymnastics',
-    date: 1761206400
+    date: 1761206400000
   },
 ] 
