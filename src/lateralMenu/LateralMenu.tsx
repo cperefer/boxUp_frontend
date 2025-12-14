4bfa2fac3044f093f1a533a0862459807850112b
Miguel <migui.kaos14@gmail.com>
2025-12-14 19:38:27 +0100

    feat: created lateral menu contact component

--- src/lateralMenu/LateralMenu.tsx
+++ src/lateralMenu/LateralMenu.tsx
@@ -1,6 +1,7 @@
 import { useMainStore } from "@/store/mainStore";
 import { ProfileData } from "./components/ProfileData";
 import { LateralMenuLinks } from "./components/LateralMenuLinks";
+import { LateralMenuContact } from "./components/LateralMenuContact";
 
 export const LateralMenu = () => {
   const { isMenuOpen } = useMainStore();
@@ -9,6 +10,7 @@ export const LateralMenu = () => {
     <div className={`bg-primary text-white z-1 h-dvh w-[50%] md:w-[30%] pt-10 px-2 transition-all duration-300 ease-in-out fixed top-0 ${isMenuOpen ? 'left-0' : '-left-400'}`}>
       <ProfileData />
       <LateralMenuLinks />
+      <LateralMenuContact />
     </div>
   )
 }
