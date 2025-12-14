ddec170c48fd21bc85b6a3940d0900e818808f9a
Miguel <migui.kaos14@gmail.com>
2025-12-14 10:37:50 +0100

    feat: refactored folder for lateralMenu

--- src/lateralMenu/LateralMenu.tsx
+++ src/lateralMenu/LateralMenu.tsx
@@ -1,10 +1,12 @@
 import { useMainStore } from "@/store/mainStore";
+import { ProfileData } from "./components/ProfileData";
 
 export const LateralMenu = () => {
   const { isMenuOpen } = useMainStore();
 
   return (
     <div className={`bg-primary z-1 h-dvh w-[50%] md:w-[30%] pt-10 px-2 transition-all duration-300 ease-in-out fixed top-0 ${isMenuOpen ? 'left-0' : '-left-400'}`}>
+      <ProfileData />
     </div>
   )
 }
