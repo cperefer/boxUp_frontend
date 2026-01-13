be40a0b45a7961e5dd773ac1467ee6f04b68e485
Miguel <migui.kaos14@gmail.com>
2026-01-13 21:25:35 +0100

    feat: manage form with react hook form

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,34 +1,65 @@
 import { CustomButton } from "@/components/custom/CustomButton"
+import { useForm } from "react-hook-form";
 import { Link } from "react-router"
 
 export const LoginPage = () => {
+  const { register, handleSubmit, formState: { errors }, } = useForm<IFormInput>()
+
+  const onSubmit = () => {
+    console.log('sent')
+  }
+
   return (
     <div className="w-5/6 xl:w-2/3 flex flex-col xl:flex-row justify-center">
       <div className="w-full xl:w-1/2 h-4/6 md:h-full flex flex-col px-10 bg-white relative">
         <p className="text-2xl font-bold py-3">Bienvenido de nuevo!</p>
         <p className="text-lg pb-3">Inicia sesión para acceder a tu información</p>
-        <form className="flex flex-col">
+        <form className="flex flex-col" onSubmit={() => console.log('submit')}>
           <div className="w-9/10 md:w-4/5 flex flex-col pb-3">
             <label htmlFor='email'>Correo electrónico</label>
             <input
+              {...register("email", {
+                required: "El correo no puede estar vacío",
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "Introduce un email válido"
+                }
+              })}
               id='email'
-              className="h-8 pl-[26px] rounded-lg border-2 bg-[url(/icons/letter.png)] bg-position-[2px] bg-no-repeat bg-size-[20px]"
+              className={`h-8 pl-[26px] rounded-lg bg-[url(/icons/letter.png)] bg-position-[2px] bg-no-repeat bg-size-[20px] ${errors.email ? 'border-red-500 border-3' : 'border-2'}`}
               placeholder="Introduce tu correo"
               type="text"
             />
+            {
+              errors.email && (
+                <p className="text-red-500 text-sm">{errors.email.message}</p>
+              )
+            }
           </div>
           <div className="w-9/10 md:w-4/5 flex flex-col pb-3">
             <label htmlFor='password'>Contraseña</label>
             <input
+              {...register("password", {
+                required: "La contraseña no puede estar vacía",
+                minLength: {
+                  value: 4,
+                  message: "La longitud mínima es 4"
+                }
+              })}
               id='password'
-              className=" h-8 pl-6 rounded-lg border-2 bg-[url(/icons/lock.png)] bg-position-[2px] bg-no-repeat bg-size-[20px]"
+              className={`h-8 pl-6 rounded-lg border-2 bg-[url(/icons/lock.png)] bg-position-[2px] bg-no-repeat bg-size-[20px] ${errors.email ? 'border-red-500 border-3' : 'border-2'}`}
               placeholder="Password"
               type="password"
             />
+            {
+              errors.password && (
+                <p className="text-red-500 text-sm">{errors.password.message}</p>
+              )
+            }
             <p className="text-sm text-right py-1"><Link to='/'>Olvidaste tu contraseña?</Link></p>
           </div>
           <div className="w-9/10 md:w-4/5 flex flex-col pb-3">
-            <CustomButton type="auth" size='fit' action={() => console.log('first')}>Iniciar sesión</CustomButton>
+            <CustomButton type="auth" size='fit' action={handleSubmit(onSubmit)}>Iniciar sesión</CustomButton>
           </div>
         </form>
         <p className="text-sm md:text-lg absolute bottom-1">¿No tienes una cuenta? <Link to='/'>Contacta con tu box.</Link></p>
