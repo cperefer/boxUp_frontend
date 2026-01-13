f1023a9a3230961d1efbc5c0020c74502ca9c9ca
Miguel <migui.kaos14@gmail.com>
2026-01-13 20:50:02 +0100

    feat: increased input size

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -8,16 +8,16 @@ export const LoginPage = () => {
         <p className="text-2xl font-bold py-3">Bienvenido de nuevo!</p>
         <p className="text-lg pb-3">Inicia sesión para acceder a tu información</p>
         <form className="flex flex-col">
-          <div className="w-5/6 md:w-4/5 flex flex-col pb-3">
+          <div className="w-9/10 md:w-4/5 flex flex-col pb-3">
             <label htmlFor='email'>Correo electrónico</label>
             <input
               id='email'
-              className=" h-8 pl-[26px] rounded-lg border-2 bg-[url(/icons/letter.png)] bg-position-[2px] bg-no-repeat bg-size-[20px]"
+              className="h-8 pl-[26px] rounded-lg border-2 bg-[url(/icons/letter.png)] bg-position-[2px] bg-no-repeat bg-size-[20px]"
               placeholder="Introduce tu correo"
               type="text"
             />
           </div>
-          <div className="w-5/6 md:w-4/5 flex flex-col pb-3">
+          <div className="w-9/10 md:w-4/5 flex flex-col pb-3">
             <label htmlFor='password'>Contraseña</label>
             <input
               id='password'
@@ -27,7 +27,7 @@ export const LoginPage = () => {
             />
             <p className="text-sm text-right py-1"><Link to='/'>Olvidaste tu contraseña?</Link></p>
           </div>
-          <div className="w-5/6 md:w-4/5 flex flex-col pb-3">
+          <div className="w-9/10 md:w-4/5 flex flex-col pb-3">
             <CustomButton type="auth" size='fit' action={() => console.log('first')}>Iniciar sesión</CustomButton>
           </div>
         </form>
