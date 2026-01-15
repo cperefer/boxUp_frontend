cb0e49a000ac5185fc127d1ea9da483e7d01b863
Miguel <migui.kaos14@gmail.com>
2026-01-15 16:29:21 +0100

    feat: changed hard code for first name for user info

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,7 +1,8 @@
 export interface User {
   id:       string;
   email:    string;
-  fullName: string;
+  firstName: string;
+  lastName: string;
   hasActiveAccount: boolean;
   role:    string;
 }
