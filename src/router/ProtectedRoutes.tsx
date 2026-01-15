e81436409571efd9150908596c6e9ea3970ee6ea
Miguel <migui.kaos14@gmail.com>
2026-01-15 10:45:50 +0100

    feat: added protected routes

---
+++ src/router/ProtectedRoutes.tsx
@@ -0,0 +1,45 @@
+import { useAuthStore } from "@/store/authStore";
+import type { PropsWithChildren } from "react";
+import { Navigate } from "react-router";
+
+export const AuthenticatedRoutes = ({ children }: PropsWithChildren) => {
+  const { status } = useAuthStore();
+
+  if (status === 'checking') {
+    return null;
+  }
+
+  if (status === 'not-logged') {
+    return <Navigate to='/login' />;
+  }
+
+  return children;
+}
+
+export const CoachRoutes = ({ children }: PropsWithChildren) => {
+  const { status, user } = useAuthStore();
+
+  if (status === 'checking') {
+    return null;
+  }
+
+  if (status === 'not-logged' || (user?.role !== 'admin' && user?.role !== 'coach')) {
+    return <Navigate to='/dashboard' />;
+  }
+
+  return children;
+}
+
+export const AdminRoutes = ({ children }: PropsWithChildren) => {
+  const { status, user } = useAuthStore();
+
+  if (status === 'checking') {
+    return null;
+  }
+
+  if (status === 'not-logged' || user?.role !== 'admin') {
+    return <Navigate to='/dashboard' />;
+  }
+
+  return children;
+}
