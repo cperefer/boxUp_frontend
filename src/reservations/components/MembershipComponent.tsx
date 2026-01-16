5a80a55af9ef3cacc31a70def54f23fbea4acc2a
Miguel <migui.kaos14@gmail.com>
2026-01-16 09:01:19 +0100

    feat: created membership component

---
+++ src/reservations/components/MembershipComponent.tsx
@@ -0,0 +1,13 @@
+export const MembershipComponent = () => {
+  return (
+    <div className="px-5 pt-2">
+      <div className="p-2 w-fit border-2 border-primary rounded-lg">
+        <p className="text-xl font-bold">Tu suscripción</p>
+        <p className="">Cuota ilimitada</p>
+        <p>Clases semanales: 5</p>
+        <p>Tu cuota incluye: CrossFit, Gymnastics, Haltero, OpenBox</p>
+        <p>Cuota disponible hasta: 31/01/2026</p>
+      </div>
+    </div>
+  )
+}
--- src/reservations/pages/ReservationsPage.tsx
+++ src/reservations/pages/ReservationsPage.tsx
@@ -1,7 +1,17 @@
+import { MembershipComponent } from "../components/MembershipComponent"
 
 export const ReservationsPage = () => {
 
   return (
-    <div>ReservationPage</div>
+    // info (creditos, tarifa, validez...)
+    // Contenedor de reservas ordenadas por fecha, 3 / 4 columnas desktop 1 mobile
+    // Cartas, en el 1/4 izquierdo un icono y un color para diferenciar clase. A la derecha clase, fecha y hora y estado (cancelada, consume credito si aplica)
+    // Implementar throttling?
+    <div className="pt-10">
+      <p className="font-bold text-2xl pl-5 pt-2">Mis reservas</p>
+      <div>
+        <MembershipComponent />
+      </div>
+    </div>
   )
 }
