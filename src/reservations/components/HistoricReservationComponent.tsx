c55253c9d851765cedc13a4b36502b0081ffd197
Miguel <migui.kaos14@gmail.com>
2026-01-18 19:39:58 +0100

    feat: added title to cards

--- src/reservations/components/HistoricReservationComponent.tsx
+++ src/reservations/components/HistoricReservationComponent.tsx
@@ -23,12 +23,9 @@ export const HistoricReservationComponent = () => {
       <p className="text-xl font-bold">Histórico de reservas</p>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2 pr-5">
         {
-          data.length && data.map(({ type, date }) => {
-            console.log(type)
-            return (
-              <CardReservationComponent type={type} date={date} />
-            )
-          })
+          data.length && data.map(({ type, date, title }) => (
+            <CardReservationComponent type={type} date={date} title={title} />
+          ))
         }
       </div>
     </div>
