ac06a5719fce617cbf6d04faaf684541c05766d8
Miguel <migui.kaos14@gmail.com>
2026-01-18 13:06:56 +0100

    feat: removed comments

--- src/reservations/pages/ReservationsPage.tsx
+++ src/reservations/pages/ReservationsPage.tsx
@@ -1,16 +1,14 @@
+import { HistoricReservationComponent } from "../components/HistoricReservationComponent"
 import { MembershipComponent } from "../components/MembershipComponent"
 
 export const ReservationsPage = () => {
 
   return (
-    // info (creditos, tarifa, validez...)
-    // Contenedor de reservas ordenadas por fecha, 3 / 4 columnas desktop 1 mobile
-    // Cartas, en el 1/4 izquierdo un icono y un color para diferenciar clase. A la derecha clase, fecha y hora y estado (cancelada, consume credito si aplica)
-    // Implementar throttling?
-    <div className="pt-10">
-      <p className="font-bold text-2xl pl-5 pt-2">Mis reservas</p>
+    <div className="pt-10 pl-5">
+      <p className="font-bold text-2xl pt-2">Mis reservas</p>
       <div>
         <MembershipComponent />
+        <HistoricReservationComponent />
       </div>
     </div>
   )
