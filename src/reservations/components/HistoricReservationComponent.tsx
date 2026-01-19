f204555f4882a7e28bdb8140347fedfccdb1ff7c
Miguel <migui.kaos14@gmail.com>
2026-01-19 17:27:59 +0100

    feat: added skeleton for reservations

--- src/reservations/components/HistoricReservationComponent.tsx
+++ src/reservations/components/HistoricReservationComponent.tsx
@@ -1,6 +1,8 @@
 import { useAuthStore } from "@/store/authStore";
 import { CardReservationComponent } from "./CardReservationComponent"
 import { useReservations } from "@/hooks/useReservations";
+import 'react-loading-skeleton/dist/skeleton.css';
+import { CardSkeleton } from "./CardSkeleton";
 
 export const HistoricReservationComponent = () => {
   const { user } = useAuthStore();
@@ -11,19 +13,17 @@ export const HistoricReservationComponent = () => {
 
   const { data, loading } = useReservations(user?.id)
 
-  if (loading) {
-    return (
-      <p>La están peinando</p>
-    )
-  }
-
-
   return (
     <div>
       <p className="text-xl font-bold">Histórico de reservas</p>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2 pr-5">
         {
-          data.length && data.map(({ type, date, title }) => (
+          loading && (
+            <CardSkeleton cardItems={8} />
+          )
+        }
+        {
+          !loading && data.length && data.map(({ type, date, title }) => (
             <CardReservationComponent type={type} date={date} title={title} />
           ))
         }
