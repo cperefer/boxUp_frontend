394de8f613a920af97cb0ee9c09a78a417989d09
Miguel <migui.kaos14@gmail.com>
2026-01-21 08:54:42 +0100

    fix: fixed text

--- src/dashboard/components/NextReservations.tsx
+++ src/dashboard/components/NextReservations.tsx
@@ -5,7 +5,7 @@ import { ReservationComponent } from "./ReservationComponent"
 export const NextReservations = () => {
   return (
     <section className="flex flex-col items-center py-2 w-full md:w-2/5">
-      <p className="text-xl">Próximas reservas</p>
+      <p className="text-xl">Últimas reservas</p>
       <div className="w-full px-2">
         {
           NextReservationsMock.map((reservation: Reservation) => (
