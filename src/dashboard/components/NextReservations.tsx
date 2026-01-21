fdb8ffbc540616dc1bd329ed4194e0f289c02d61
Miguel <migui.kaos14@gmail.com>
2026-01-21 08:58:06 +0100

    feat: added useReservations for dashboard

--- src/dashboard/components/NextReservations.tsx
+++ src/dashboard/components/NextReservations.tsx
@@ -1,14 +1,23 @@
-import { NextReservationsMock } from "@mocks/NextReservations.mock"
 import type { Reservation } from "@/interfaces/Classes";
 import { ReservationComponent } from "./ReservationComponent"
+import { useAuthStore } from "@/store/authStore";
+import { useReservations } from "@/hooks/useReservations";
 
 export const NextReservations = () => {
+  const { user } = useAuthStore();
+
+  if (!user) {
+    return;
+  }
+
+  const { data, loading } = useReservations(user?.id)
+
   return (
     <section className="flex flex-col items-center py-2 w-full md:w-2/5">
       <p className="text-xl">Últimas reservas</p>
       <div className="w-full px-2">
         {
-          NextReservationsMock.map((reservation: Reservation) => (
+          data.map((reservation: Reservation) => (
             <ReservationComponent key={reservation.id} reservation={reservation} />
           ))
         }
