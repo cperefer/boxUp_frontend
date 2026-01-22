63fb2726ad345ceba3c0cc99ee9453cd6c03fb18
Miguel <migui.kaos14@gmail.com>
2026-01-22 17:41:52 +0100

    feat: added navigation to class

--- src/components/SeeCancelReservationButtons.tsx
+++ src/components/SeeCancelReservationButtons.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import { FiEye, FiTrash } from 'react-icons/fi'
 import { CustomButton } from './custom/CustomButton'
 import { useCancelReservation } from '@/hooks/useCancelReservation'
+import { useNavigate } from 'react-router';
 
 interface Props {
   id: number;
@@ -11,13 +11,14 @@ interface Props {
 const cancelReservationDeadline = 1000 * 10 * 60 * 3
 
 export const SeeCancelReservationButtons = ({ id, date }: Props) => {
+  const navigate = useNavigate();
   const handleDelete = () => {
     useCancelReservation(id);
   }
 
   return (
     <>
-      <CustomButton type="success" action={() => { }} size='sm'><FiEye /></CustomButton>
+      <CustomButton type="success" action={() => navigate(`/classes/${id}`)} size='sm'><FiEye /></CustomButton>
       {
         (date - cancelReservationDeadline > Date.now()) && (
           <CustomButton type="error" action={handleDelete} size='sm'><FiTrash /></CustomButton>
