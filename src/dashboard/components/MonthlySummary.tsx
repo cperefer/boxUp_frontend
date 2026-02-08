36ec9eac53da341af606ac1a0756af5dd8f4945a
Miguel <migui.kaos14@gmail.com>
2026-02-08 10:11:15 +0100

    feat: added loading skeleton

--- src/dashboard/components/MonthlySummary.tsx
+++ src/dashboard/components/MonthlySummary.tsx
@@ -1,6 +1,7 @@
 import { useMonthlyStats } from "@/hooks/useMonthlyStats";
 import { useAuthStore } from "@/store/authStore";
 import { statsIconMap } from "@/utils/statsIconMap";
+import Skeleton from "react-loading-skeleton";
 
 export const MonthlySummary = () => {
   const { user } = useAuthStore();
@@ -9,17 +10,19 @@ export const MonthlySummary = () => {
   return (
     <section className="flex flex-col items-center py-2 w-full md:w-2/5">
       <p className="text-xl">Resumen mensual</p>
-      <ul>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          data.map((stat) => (
+      {loading ? (
+        <div className="w-2/3 h-full">
+          <Skeleton height={20} count={6}></Skeleton>
+        </div>
+      ) : (
+        <ul>
+          {data.map((stat) => (
             <li key={stat.type}>
               {statsIconMap[stat.type]} {stat.description}
             </li>
-          ))
-        )}
-      </ul>
+          ))}
+        </ul>
+      )}
     </section>
   );
 };
