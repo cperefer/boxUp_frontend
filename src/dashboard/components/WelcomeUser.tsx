d2917a7e3d2d030d2d5a6aca3873a6756695c915
Miguel <migui.kaos14@gmail.com>
2026-02-10 21:34:18 +0100

    feat: changed where reservation data is obtained for dashboard

--- src/dashboard/components/WelcomeUser.tsx
+++ src/dashboard/components/WelcomeUser.tsx
@@ -1,14 +1,33 @@
 import { CustomProfilePicture } from "@/components/custom/CustomProfilePicture";
+import type { Reservation } from "@/interfaces/Classes";
 import { useAuthStore } from "@/store/authStore";
+import { getNextReservation } from "@/utils/getNextReservation";
+import { parseDate } from "@/utils/parseDate";
 
-export const WelcomeUser = () => {
+interface Props {
+  data: Reservation[];
+  loading: boolean;
+}
+export const WelcomeUser = ({ data, loading }: Props) => {
   const { user } = useAuthStore();
+  let reservation = null;
+
+  if (!loading) {
+    reservation = getNextReservation(data);
+  }
+
   return (
     <section className="flex flex-col items-center bg-gray-700 bg-[url(/profile_bg.jpg)] bg-position-[2px] bg-no-repeat bg-cover w-full drop-shadow-xl pb-2">
       <CustomProfilePicture />
       <p className="text-2xl pb-2 text-white">Bienvenido, {user?.firstName}!</p>
       <p className="text-white">
-        Tu próxima reserva es el 28/10/2025 a las 18:30
+        {loading ? (
+          <span className="invisible">cargando</span>
+        ) : (
+          reservation && (
+            <>Tu próxima reserva es {parseDate(reservation.date, true)}</>
+          )
+        )}
       </p>
     </section>
   );
--- src/dashboard/pages/DashboardPage.tsx
+++ src/dashboard/pages/DashboardPage.tsx
@@ -1,15 +1,20 @@
-import { MonthlySummary } from "../components/MonthlySummary"
-import { NextReservations } from "../components/NextReservations"
-import { SubscriptionStatus } from "../components/SubscriptionStatus"
-import { UpcomingEvents } from "../components/UpcomingEvents"
-import { WelcomeUser } from "../components/WelcomeUser"
+import { useAuthStore } from "@/store/authStore";
+import { MonthlySummary } from "../components/MonthlySummary";
+import { NextReservations } from "../components/NextReservations";
+import { SubscriptionStatus } from "../components/SubscriptionStatus";
+import { UpcomingEvents } from "../components/UpcomingEvents";
+import { WelcomeUser } from "../components/WelcomeUser";
+import { useReservations } from "@/hooks/useReservations";
 
 export const DashboardPage = () => {
+  const { user } = useAuthStore();
+  const { data, loading } = useReservations(user?.id);
+
   return (
     <div className="pt-10">
-      <WelcomeUser />
+      <WelcomeUser data={data} loading={loading} />
       <div className="flex flex-col md:flex-row md:justify-around pb-2">
-        <NextReservations />
+        <NextReservations data={data} loading={loading} />
         <MonthlySummary />
       </div>
       <div className="flex flex-col md:flex-row md:justify-around">
@@ -17,5 +22,5 @@ export const DashboardPage = () => {
         <UpcomingEvents />
       </div>
     </div>
-  )
-}
+  );
+};
